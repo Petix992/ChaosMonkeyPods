4153246d29dbf5cace6ba16f28b7b2a1d48b47a0
Francesco Petillo <francesco.petillo@ericsson.com>
2023-03-01 17:21:06 +0100

    Upload Project

---
+++ myproject/src/main/java/com/myproject/App.java
@@ -0,0 +1,21 @@
+package com.myproject;
+
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App 
+{
+    public static void main( String[] args ){
+        
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
+
+
+            client.pods().inNamespace("testing").delete();
+            
+        }
+    }
+}
